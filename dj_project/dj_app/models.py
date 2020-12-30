a8a148a7b5b15c05fa906e1b30627f3b46dc12f7
m-fhm <joinfa333@yahoo.com>
2020-12-30 13:49:59 +0500

    admin register

--- dj_project/dj_app/models.py
+++ dj_project/dj_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Sutdent(models.Model):
+class Student(models.Model):
+    name= models.CharField(max_length=100)
+
+class  Teacher(models.Model):
     name= models.CharField(max_length=100)
 
