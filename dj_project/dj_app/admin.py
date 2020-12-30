1885fd7371a235787b348a23a768955c03906dfa
Muhammad Hazaifa <hazaifa.awan@gmail.com>
2020-12-30 14:23:00 +0500

    new tables

--- dj_project/dj_app/admin.py
+++ dj_project/dj_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from dj_app.models import Student,Teacher
+from dj_app.models import post, comment
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Teacher)
+#admin.site.register(Student)
+#admin.site.register(Teacher)
+admin.site.register(post)
+admin.site.register(comment)
+
+
--- dj_project/dj_app/models.py
+++ dj_project/dj_app/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
+from datetime import datetime
 # Create your models here.
-class Student(models.Model):
-    name= models.CharField(max_length=100)
+class post (models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=datetime.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-class  Teacher(models.Model):
-    name= models.CharField(max_length=100)
-
+class comment (models.Model):
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
